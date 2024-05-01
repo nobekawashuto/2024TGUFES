d310ae79cd50a854bacfaeebdc2e5e8c686d9ba2
nobekawashuto <maybeWinn1ble@gmail.com>
2024-05-01 17:16:25 +0900

    画像の挿入色々試してみてます。

--- tgufes/src/pages/index.tsx
+++ tgufes/src/pages/index.tsx
@@ -7,23 +7,25 @@ export default function Home() {
   let test = 'hello world'
   return (
     <>
+    <div id="home" class="big-bg">
     <header className="page-header wrapper">
-      <h2><a href="index.html">東北学院大学祭公式WEBサイト</a></h2>
+      <h2>東北学院大学祭公式WEBサイト</h2>
       <nav>
         <ul className="main-nav">
-          <li><a href="test1.html">芸能人企画</a></li>
-          <li><a href="test2.html">スケジュール・マップ</a></li>
-          <li><a href="test3.html">協賛企業様一覧</a></li>
+          <li><a href="index.html">トップページ</a></li>
+          <li><a href="test2.html">芸能人企画</a></li>
+          <li><a href="test3.html">スケジュール</a></li>
           <li><a href="contact.html">お問い合わせ</a></li>
         </ul>
       </nav>
     </header>
-    <div className="home-content wrapper">
+    {/* <div className="home-content wrapper">
       <h1 className="page-title">#TGUFES</h1>
       <p>10月14日(月・祝)10:00〜:五橋祭 五橋キャンパス<br/>10月26日(土)10:00〜:六軒丁祭 土樋キャンパス</p>
       <a className="button" href="test2.html">大学祭についてはこちら！</a><br/><br/>
       <p>大学祭開催まで残り<span id="day"></span>日</p>
     
+    </div> */}
     </div>
     <div className="sns">
     <h2 className="sns-title">SNS</h2>
