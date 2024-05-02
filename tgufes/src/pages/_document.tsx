622bc17af5e93089bc2e20a2d3d27207818aee8b
nobekawashuto <maybeWinn1ble@gmail.com>
2024-05-02 14:34:16 +0900

    フォントの変更を行いました

--- tgufes/src/pages/_document.tsx
+++ tgufes/src/pages/_document.tsx
@@ -20,14 +20,9 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
-          />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Monomaniac+One&display=swap" rel="stylesheet"></link>
-          
+          <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+          <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+          <link href="https://fonts.googleapis.com/css2?family=Shippori+Mincho+B1&display=swap" rel="stylesheet"></link>
         </Head>
         <body>
           <Main />
