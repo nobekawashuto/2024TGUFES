b1825716f6c52ad1dd036a562de6527b11c11d44
nobekawashuto <maybeWinn1ble@gmail.com>
2024-05-03 18:34:41 +0900

    スライドショーを試しました

--- tgufes/src/pages/test.tsx
+++ tgufes/src/pages/test.tsx
@@ -1,10 +1,21 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import HeaderMenu from "@/components/headerMenu";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <h1>Test</h1>
+    <>
+
+      <div className="slider">
+        <div className="image"><HeaderMenu /></div>
+        <div className="image"><HeaderMenu /></div>
+        <div className="image"><HeaderMenu /></div>
+      </div>
+
+
+    </>
+
   );
 }
