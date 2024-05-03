81a66334e99b2413c1095f28f098d205a310e006
nobekawashuto <maybeWinn1ble@gmail.com>
2024-05-04 01:38:32 +0900

    test.tsxでビデオタグを試しています

--- tgufes/src/pages/test.tsx
+++ tgufes/src/pages/test.tsx
@@ -8,12 +8,27 @@ export default function Home() {
   return (
     <>
 
-      <div className="slider">
-        <div className="image"><HeaderMenu /></div>
-        <div className="image"><HeaderMenu /></div>
-        <div className="image"><HeaderMenu /></div>
-      </div>
+      <header id="header">
+        <HeaderMenu />
+        <div id="video-area">
+          <video id="video" webkit-playsinline playsInline muted autoPlay loop controls/>
+          <source src="https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/6-3-1/video/movie.mp4" type="video/mp4" />
+          
+          {/* poster：動画ファイルが利用できない環境で代替表示される画像
+            webkit-playsinline：iOS 9までのSafari用インライン再生指定
+            playsinline：iOS 10以降のSafari用インライン再生指定
+            muted：音声をミュートさせる
+            autoplay：動画を自動再生させる
+            loop：動画をループさせる
+            controls：コントロールバーを表示する */}
 
+
+        </div>
+      </header>
+
+      <div id="container">
+        <p>ここにコンテンツが入ります</p>
+      </div>
 
     </>
 
