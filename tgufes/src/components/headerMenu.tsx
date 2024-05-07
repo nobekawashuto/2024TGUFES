5587c5180ac2afa41afca1ad1368b6fd18ab64b2
nobekawashuto <maybeWinn1ble@gmail.com>
2024-05-07 14:50:46 +0900

    HeaderMenuを修正しました

--- tgufes/src/components/headerMenu.tsx
+++ tgufes/src/components/headerMenu.tsx
@@ -9,7 +9,7 @@ const HeaderMenu = () => {
       <h2>東北学院大学祭公式WEBサイト</h2>
       <nav>
         <ul className="main-nav">
-          <li><Link href="/index">トップページ</Link></li>
+          <li><Link href="/">トップページ</Link></li>
           <li><a href="test2.html">芸能人企画</a></li>
           <li><a href="test3.html">スケジュール</a></li>
           <li><Link href="/forms">お問い合わせ</Link></li>
