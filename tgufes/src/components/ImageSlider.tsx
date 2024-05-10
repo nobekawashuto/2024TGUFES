1cdd2f8f6eb2498056c23e129c011dfa51ca5cc3
nobekawashuto <maybeWinn1ble@gmail.com>
2024-05-11 00:57:00 +0900

    画像を調整しました

--- tgufes/src/components/ImageSlider.tsx
+++ tgufes/src/components/ImageSlider.tsx
@@ -13,7 +13,7 @@ function ImageSlider() {
   };
 
   return (
-    <div style={{width: "100%", height: "700px"}}>
+    <div style={{width: "100%", height: "1000px"}}>
       <Slider {...settings}>
         <div>
           <Image src="/images/IMG_3323.JPG" alt="Image 1" height={1000} width={1000} layout="responsive"/>
