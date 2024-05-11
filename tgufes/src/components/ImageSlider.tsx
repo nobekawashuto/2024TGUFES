ebf66a194fbacd08eff4463380b3ba67dee340ba
nobekawashuto <maybeWinn1ble@gmail.com>
2024-05-12 00:50:40 +0900

    画像の切り替わりをフェードに変更しました。

--- tgufes/src/components/ImageSlider.tsx
+++ tgufes/src/components/ImageSlider.tsx
@@ -11,28 +11,32 @@ function ImageSlider() {
     autoplay: true,
     autoplaySpeed: 5000, // 表示している秒数
     pauseOnHover:false,
+    fade:true,
   };
 
   return (
     <div className="slider">
       <Slider {...settings}>
         <div>
-          <Image src="/images/IMG_3323.JPG" alt="Image 1" height={1000} width={1000} layout="responsive"/>
+          <Image src="/images/IMG_3323.JPG" alt="Image 3" height={1000} width={1000} layout="responsive"/>
         </div>
         <div>
           <Image src="/images/IMG_3384.JPG" alt="Image 2" height={1000} width={1000} layout="responsive"/>
         </div>
         <div>
-          <Image src="/images/DSC00316.JPG" alt="Image 3" height={1000} width={1000} layout="responsive"/>
+          <Image src="/images/DSC00316.JPG" alt="Image 4" height={1000} width={1000} layout="responsive"/>
         </div>
         <div>
-          <Image src="/images/IMG_5551.JPG" alt="Image 4" height={1000} width={1000} layout="responsive"/>
+          <Image src="/images/IMG_5551.JPG" alt="Image 5" height={1000} width={1000} layout="responsive"/>
         </div>
         <div>
-          <Image src="/images/IMG_7858.jpeg" alt="Image 5" height={1000} width={1000} layout="responsive"/>
+          <Image src="/images/IMG_7858.jpeg" alt="Image 6" height={1000} width={1000} layout="responsive"/>
         </div>
         <div>
-          <Image src="/images/IMG_5767.JPG" alt="Image 6" height={1000} width={1000} layout="responsive"/>
+          <Image src="/images/IMG_5767.JPG" alt="Image 7" height={1000} width={1000} layout="responsive"/>
+        </div>
+        <div>
+          <Image src="/images/IMG_7860.JPG" alt="Image 1" height={1000} width={1000} layout="responsive"/>
         </div>
       </Slider>
     </div>
