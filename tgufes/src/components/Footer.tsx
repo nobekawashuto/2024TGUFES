08c6498b04211e46ec3f6ae0ef6e9fbb57251a3c
nobekawashuto <maybeWinn1ble@gmail.com>
2024-05-17 15:35:07 +0900

    topページに戻るセクションをFooterのコンポーネント内に合体させました

--- tgufes/src/components/Footer.tsx
+++ tgufes/src/components/Footer.tsx
@@ -5,11 +5,16 @@ type Props = {}
 
 const Footer = () => {
     return (
-        <footer>
-            <div className="wrapper">
-                <p><small>&copy;2024年度東北学院大学祭実行委員会HP制作班</small></p>
+        <>
+            <div className="backtop">
+                <h4><Link href="/">トップに戻る</Link></h4>
             </div>
-        </footer>
+            <footer>
+                <div className="wrapper">
+                    <p><small>&copy;2024年度東北学院大学祭実行委員会HP制作班</small></p>
+                </div>
+            </footer>
+        </>
     )
 }
 
--- tgufes/src/pages/index.tsx
+++ tgufes/src/pages/index.tsx
@@ -56,9 +56,9 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="backtop">
+      {/* <div className="backtop">
         <h4><Link href="/">トップに戻る</Link></h4>
-      </div>
+      </div> */}
       {/* <footer>
         <div className="wrapper">
           <p><small>&copy;2024年度東北学院大学祭実行委員会HP制作班</small></p>
