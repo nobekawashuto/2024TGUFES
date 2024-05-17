02a9904896e29ba784b940e0dd66e5fb83924960
nobekawashuto <maybeWinn1ble@gmail.com>
2024-05-17 15:30:28 +0900

    ヘッダー部分の色を変更しました

--- tgufes/src/components/headerMenu.tsx
+++ tgufes/src/components/headerMenu.tsx
@@ -9,10 +9,10 @@ const HeaderMenu = () => {
       <h2>東北学院大学祭公式WEBサイト</h2>
       <nav>
         <ul className="main-nav">
-          <li><Link href="/">トップページ</Link></li>
-          <li><Link href="/comedy">芸能人企画</Link></li>
-          <li><a href="test3.html">スケジュール</a></li>
-          <li><Link href="/forms">お問い合わせ</Link></li>
+          <li><Link className='mainLink' href="/">トップページ</Link></li>
+          <li><Link className='mainLink' href="/comedy">芸能人企画</Link></li>
+          <li><a className='mainLink' href="test3.html">スケジュール</a></li>
+          <li><Link className='mainLink' href="/forms">お問い合わせ</Link></li>
         </ul>
       </nav>
     </div>
