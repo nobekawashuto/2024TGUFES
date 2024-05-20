8e783fb50fba5b98ae3f1ad664829ceac46894b0
nobekawashuto <maybeWinn1ble@gmail.com>
2024-05-20 15:18:00 +0900

    お問い合わせページにおけるスライドショーの実装とGoogleFormの高さ調整を行いました

--- tgufes/src/pages/forms.tsx
+++ tgufes/src/pages/forms.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Link from 'next/link';
 import HeaderMenu from "@/components/headerMenu";
+import ImageSlider from "@/components/ImageSlider";
 import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -9,13 +10,13 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
     return (
         <>
-            <div id="home" className="big-bg">
-                <HeaderMenu />
-            </div>
+
+            <HeaderMenu />
+            <ImageSlider/>
             <div className="form">
-                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScu1xACysLkLIt05JBHu2Cc0-8WekMSaSujyIb60YWT8ZOzWw/viewform?embedded=true" className="goo-form"width={1240} height={729} >読み込んでいます…</iframe>
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScu1xACysLkLIt05JBHu2Cc0-8WekMSaSujyIb60YWT8ZOzWw/viewform?embedded=true" className="goo-form" width={1240} height={900} >読み込んでいます…</iframe>
             </div>
-            <Footer/>
+            <Footer />
         </>
 
 
