e5267e9c21554031e0c087261ec3b7447fa7881b
nobekawashuto <maybeWinn1ble@gmail.com>
2024-06-06 19:25:29 +0900

    オープニングアニメションの細やかな修正、レスポンシブデザインの対応を行いました

--- tgufes/src/components/OpeningAnimation.tsx
+++ tgufes/src/components/OpeningAnimation.tsx
@@ -2,13 +2,14 @@
 import { motion } from 'framer-motion';
 import { useEffect, useState } from 'react';
 
+
 const OpeningAnimation = () => {
   const [isVisible, setIsVisible] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsVisible(false);
-    }, 3000); // 3秒後にアニメーションを終了
+    }, 4000); // 2秒後にアニメーションを終了
 
     return () => clearTimeout(timer);
   }, []);
@@ -18,7 +19,7 @@ const OpeningAnimation = () => {
       <motion.div
         initial={{ opacity: 1 }}
         animate={{ opacity: 0 }}
-        transition={{ duration: 1.5 }}
+        transition={{delay: 2.5, duration: 1.5}}
         style={{
           position: 'fixed',
           top: 0,
@@ -27,13 +28,15 @@ const OpeningAnimation = () => {
           height: '100%',
           backgroundColor: '#000',
           display: 'flex',
+          flexDirection:'column',
           alignItems: 'center',
           justifyContent: 'center',
           color: '#fff',
           zIndex: 1000,
         }}
       >
-        <h1>Welcome to TGUFES</h1>
+        <h1 style={{ margin: 0 }} className='loadingTitle'>Welcome to TGUFES WEBSITE</h1>
+        <p style={{ margin: 0 ,fontSize:'24px'}} className='loadingTxt'>NOW LOADING ...</p>
       </motion.div>
     )
   );
