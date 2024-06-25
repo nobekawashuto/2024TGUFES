fb897ee209293034f9e957a3b37bec2243e732da
nobekawashuto <maybeWinn1ble@gmail.com>
2024-06-25 15:43:51 +0900

    トピックセクションを編集しています

--- tgufes/src/components/Topics.tsx
+++ tgufes/src/components/Topics.tsx
@@ -9,7 +9,8 @@ const Topics = (props: Props) => {
         <h2 className='Topic-title'>Topics</h2>
 
         <h4 className='content-date'>2024.6.21</h4>
-        <p className='content-txt'>・2024年度東北学院大学祭のホームページversion1.0を公開しました</p>
+        <p className='content-txt'>2024年度東北学院大学祭のホームページversion1.0を公開しました</p>
+        <p className='viewmore'>→View More</p>
     </div>
     </>
   )
