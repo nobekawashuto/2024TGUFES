2bb052c5512d394984213c389d0dcbacc6bcf550
nobekawashuto <maybeWinn1ble@gmail.com>
2024-07-10 11:59:38 +0900

    Google formを更新しました

--- tgufes/src/pages/forms.tsx
+++ tgufes/src/pages/forms.tsx
@@ -14,7 +14,8 @@ export default function Home() {
             <HeaderMenu />
             <ImageSlider/>
             <div className="form">
-                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScu1xACysLkLIt05JBHu2Cc0-8WekMSaSujyIb60YWT8ZOzWw/viewform?embedded=true" className="goo-form" width={1240} height={900} >読み込んでいます…</iframe>
+                {/* <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScu1xACysLkLIt05JBHu2Cc0-8WekMSaSujyIb60YWT8ZOzWw/viewform?embedded=true" className="goo-form" width={1240} height={900} >読み込んでいます…</iframe> */}
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfjF323wIrtSpGqskZvbwpKUcJ3awSWDold6NuQThxHRTkaOw/viewform?embedded=true" className="goo-form" width={600} height={900} >読み込んでいます…</iframe>
             </div>
             <Footer />
         </>
