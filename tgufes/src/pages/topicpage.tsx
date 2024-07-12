976ba7e40fcce2c225f57e134f5a0a7468f176e3
nobekawashuto <maybeWinn1ble@gmail.com>
2024-07-12 19:37:14 +0900

    BeVoiceの変更です

--- tgufes/src/pages/topicpage.tsx
+++ tgufes/src/pages/topicpage.tsx
@@ -15,7 +15,9 @@ export default function Home() {
 
             <div className='Topics'>
                 <h4 className='topicpage-date txt'>2024.7.11<br/>あのTVの人気企画｢未成年の主張｣をオマージュした特別イベントBe.Voice開催決定!!</h4>
-                <p className='topicpage-txt txt'>この度私たちは、｢Be.Voice｣の参加者を募集しています！<br/>Be.VoiceとはあのTVの人気企画｢未成年の主張｣をオマージュした特別イベントです！<br/>土樋キャンパスで行われる六軒町祭1日目10/26（土）に開催を予定しています。<br/>あなたの思いを大勢の前で叫ぶチャンス！普段言えないあんなことやみんなに伝えたいこの気持ち、思い切り叫んでみませんか！<br/><br/>ご応募受付期間は7月8日～7月22日までです！みなさんのご応募お待ちしております！！<br/>応募は下記のURLから！ <br/><br/><a href="https://docs.google.com/forms/d/1aOYbLnpwI2I34HAk-I0MY6DegT0TfwEBYiIa8YqfEDs/viewform?pli=1&pli=1&edit_requested=true">href=https://docs.google.com/forms/d/1aOYbLnpwI2I34HAk-I0MY6DegT0TfwEBYiIa8YqfEDs/viewform?pli=1&pli=1&edit_requested=true</a></p>
+                <p className='topicpage-txt txt'>この度私たちは、｢Be.Voice｣の参加者を募集しています！<br/>Be.VoiceとはあのTVの人気企画｢未成年の主張｣をオマージュした特別イベントです！<br/>ご応募受付期間は7月8日～7月22日までです！みなさんのご応募お待ちしております！！<br/>
+                <Image src="/images/BeVoice.jpg" alt="BeVoiceImage" className="bevoiceImg"width={500}height={500} objectFit="contain" />
+                応募は下記のURLから！ <br/><br/><a href="https://docs.google.com/forms/d/1aOYbLnpwI2I34HAk-I0MY6DegT0TfwEBYiIa8YqfEDs/viewform?pli=1&pli=1&edit_requested=true">href=https://docs.google.com/forms/d/1aOYbLnpwI2I34HAk-I0MY6DegT0TfwEBYiIa8YqfEDs/viewform?pli=1&pli=1&edit_requested=true</a></p>
             </div>
 
 
