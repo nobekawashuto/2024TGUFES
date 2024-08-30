6f84acc50cf83b071a79c2fc6de88c2bd53cd1fa
nobekawashuto <maybeWinn1ble@gmail.com>
2024-08-30 14:10:18 +0900

    芸人ページ注意事項追加です、フォントも調整しました

--- tgufes/src/pages/_document.tsx
+++ tgufes/src/pages/_document.tsx
@@ -21,11 +21,12 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-          <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+          <link rel="preconnect" href="https://fonts.gstatic.com" ></link>
           <link href="https://fonts.googleapis.com/css2?family=Shippori+Mincho+B1&display=swap" rel="stylesheet"></link>
           <link href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&display=swap" rel="stylesheet"></link>
           <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet"></link>
           <link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet"></link>
+          <link href="https://fonts.googleapis.com/css2?family=Shippori+Antique+B1&display=swap" rel="stylesheet"></link>
           <link rel="icon" href="/images/HP-favicon.JPG" />
         </Head>
         <body>
--- tgufes/src/pages/veiw_comedy.tsx
+++ tgufes/src/pages/veiw_comedy.tsx
@@ -47,7 +47,7 @@ export default function Home() {
                             <div className="photo-dre"><Image src={itutuTalent}alt=""className="itutu-talent-photo"/></div>
                         </div>
                         <div className="explanation">
-                            <p className="txt talent-name">石山順征</p>
+                            <p className="talent-name">石山順征</p>
                             <p className="txt isiyama-deil">第36回ジュノン・スーパーボーイコンテスト　グランプリ受賞。<br/>15155人の頂点に輝く<br/>小学校２年生から高校卒業までサッカー中心の生活を送る。小学5年生の時にスペインレアル・マドリードでの練習に参加。<br/>高校最後の選手権で宮城県大会準優勝メンバー。<br/>大学進学と同時に上京。俳優を目指す。<br/>2024年7月期ドラマ　テレビ朝日　金曜ナイトドラマ「伝説の頭(ヘッド) 翔」　小見田 信造  役</p>
                             {/* <p className="txt comedy-deil">10月14日(月・祝)14:15 ~ 15:30</p>
                             <p className="txt comedy-deil">五橋キャンパス押川記念ホール</p> */}
@@ -60,7 +60,7 @@ export default function Home() {
                             <div className="photo-dre"><Image src={itutuDance}alt=""className="itutu-dance-photo"/></div>
                         </div>
                         <div className="explanation">
-                            <p className="txt talent-name">suzunaokada</p>
+                            <p className="talent-name">suzunaokada</p>
                             <p className="txt isiyama-deil">Singer Song Dancer｜歌 × 踊🇯🇵<br/>【2001年 12月 26日　生まれ｜東京都江戸川区　出身】<br/>ハスキーボイスを強みとし、&quot;歌 × 踊&quot;のコラボレーションを夢見る新生アーティスト。<br/>身体の柔軟性やグルーヴのある動きが特徴的なコンテンポラリーダンサー謙シンガー。<br/>ファッションや表情からは独特な雰囲気を感じさせる、不思議な世界観の持ち主。<br/>■ プロフィール<br/>ダンス歴は16年。コンテンポラリーを主ジャンルとし、2024年から本格的に歌の発信をはじめる。<br/>ダンス×歌を掛け合わせた唯一無二の色を持つアーティストとして現在活動中。<br/>■ 経歴<br/>・Anly 『Alive official video』MV出演<br/>・WORLD OF DANCE 2023   best4<br/>・岡本カウアン 大阪ツアー バックダンサー<br/>・NOA &quot;NO.A&quot; TOUR SPECIAL LIVE IN ARIAKE <br/>ARENA バックダンサー</p>
                             {/* <p className="txt comedy-deil">10月14日(月・祝)14:15 ~ 15:30</p>
                             <p className="txt comedy-deil">五橋キャンパス押川記念ホール</p> */}
@@ -75,7 +75,17 @@ export default function Home() {
                     <div className="attention">
                         <h2 className="attention-titile txt">注意事項</h2>
                         <div>
-                            <p className="attention-deil txt">・本番中は、カメラ、携帯電話などによる撮影は一切禁止となっております。<br/>・係員の指示に従って頂けない場合、公演の中断、もしくは中止をする場合があります。<br/>・駐車場がないため、公共交通機関にてお越しください。</p>
+                            <p className="attention-deil txt">どなた様も必ずご一読くださいます様、お願い申し上げます。<br/><br/>
+                                ・本企画は予約なし、整理券配布なしの先着順・自由席となっております。<br/>
+                                ・先着順となっているため、空席がない場合もございます。ご了承ください。<br/>
+                                ・駐車場がないため、公共交通機関にてお越しください。<br/>
+                                ・五橋トーク&ダンスショー、お笑いライブの参加につきまして、早朝からの順番待ちは固くお断りさせていただきます。<br/><br/>
+                                以下の事項に該当する行為を見つけた場合は口頭で注意し、改善が見られない場合は退場していただく場合がございます。<br/>
+                                ・開演中のスマートフォンやカメラ等の録画・録音機能が備わった機器の使用。<br/>
+                                ・会場内での大声での会話、来場者同士の接触。<br/>
+                                ・その他、実行委員会が迷惑行為と判断したもの。<br/>
+                                また、体調の優れない方(発熱、咳、倦怠感、味覚嗅覚障害、鼻水、くしゃみ等)のご入場はご遠慮いただく場合がございます。<br/><br/>
+                                来場者の皆様が楽しめるようなご協力をよろしくお願いいたします。</p>
                         </div>
                     </div>
                 </ScrollRevealContainer>
@@ -87,11 +97,11 @@ export default function Home() {
                 <div className="yokonarabi">
                     <div className="tutigei1">
                         <div className="photo-dre"><Image src={tutigei1}alt=""className="tutigei"/></div>
-                        <p className="gei-name txt">スカチャン</p>
+                        <p className="gei-name">スカチャン</p>
                     </div>
                     <div className="tutigei2">
                         <div className="photo-dre"><Image src={tutigei2}alt=""className="tutigei"/></div>
-                        <p className="gei-name txt">やさしいズ</p>
+                        <p className="gei-name">やさしいズ</p>
                     </div>
                 </div>
                 <div className="explanation">
@@ -101,11 +111,22 @@ export default function Home() {
                 <div className="attention">
                     <h2 className="attention-titile txt">注意事項</h2>
                     <div>
-                        <p className="attention-deil txt">・本番中は、カメラ、携帯電話などによる撮影は一切禁止となっております。<br/>・係員の指示に従って頂けない場合、公演の中断、もしくは中止をする場合があります。<br/>・駐車場がないため、公共交通機関にてお越しください。</p>
+                    <p className="attention-deil txt">どなた様も必ずご一読くださいます様、お願い申し上げます。<br/><br/>
+                                    ・本企画は予約なし、整理券配布なしの先着順・自由席となっております。<br/>
+                                    ・先着順となっているため、空席がない場合もございます。ご了承ください。<br/>
+                                    ・駐車場がないため、公共交通機関にてお越しください。<br/>
+                                    ・五橋トーク&ダンスショー、お笑いライブの参加につきまして、早朝からの順番待ちは固くお断りさせていただきます。<br/><br/>
+                                    以下の事項に該当する行為を見つけた場合は口頭で注意し、改善が見られない場合は退場していただく場合がございます。<br/>
+                                    ・開演中のスマートフォンやカメラ等の録画・録音機能が備わった機器の使用。<br/>
+                                    ・会場内での大声での会話、来場者同士の接触。<br/>
+                                    ・その他、実行委員会が迷惑行為と判断したもの。<br/>
+                                    また、体調の優れない方(発熱、咳、倦怠感、味覚嗅覚障害、鼻水、くしゃみ等)のご入場はご遠慮いただく場合がございます。<br/><br/>
+                                    来場者の皆様が楽しめるようなご協力をよろしくお願いいたします。</p>
                     </div>
                 </div>
             </ScrollRevealContainer>
             <div className="space"></div>
+            <Footer/>
 
 
 
