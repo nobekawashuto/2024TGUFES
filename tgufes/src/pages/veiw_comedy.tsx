33beab1d178cd6e9eaa51a1d1f96aa74f6d5b458
nobekawashuto <maybeWinn1ble@gmail.com>
2024-09-07 16:38:54 +0900

    芸人ページの修正とBasic認証の設定です

--- tgufes/src/pages/veiw_comedy.tsx
+++ tgufes/src/pages/veiw_comedy.tsx
@@ -38,12 +38,12 @@ export default function Home() {
             <div className="space"></div>
             <ScrollRevealContainer>
                 <div className="comedy-content">
-                    <h2 className="itutu-title txt">五橋祭トーク & ダンスショー</h2>
+                    <h2 className="itutu-title txt">五橋祭アーティストライブ & トークショー</h2>
                 </div>
-                <div className="itutu-date">
-                        <p className="txt comedy-deil">10月14日(月・祝)14:15 ~ 15:30</p>
+                {/* <div className="itutu-date">
+                        <p className="txt comedy-deil">10月14日(月・祝)14:15 ~ 14:30</p>
                         <p className="txt comedy-deil">五橋キャンパス押川記念ホール</p>
-                </div>
+                </div> */}
             </ScrollRevealContainer>
                 <ScrollRevealContainer>
                     <div className="yokonarabi">
@@ -53,8 +53,8 @@ export default function Home() {
                         <div className="explanation">
                             <p className="talent-name">石山順征</p>
                             <p className="txt isiyama-deil">第36回ジュノン・スーパーボーイコンテスト　グランプリ受賞。<br/>15155人の頂点に輝く<br/>小学校２年生から高校卒業までサッカー中心の生活を送る。小学5年生の時にスペインレアル・マドリードでの練習に参加。<br/>高校最後の選手権で宮城県大会準優勝メンバー。<br/>大学進学と同時に上京。俳優を目指す。<br/>2024年7月期ドラマ　テレビ朝日　金曜ナイトドラマ「伝説の頭(ヘッド) 翔」　小見田 信造  役</p>
-                            {/* <p className="txt comedy-deil">10月14日(月・祝)14:15 ~ 15:30</p>
-                            <p className="txt comedy-deil">五橋キャンパス押川記念ホール</p> */}
+                            <p className="txt comedy-deil">10月14日(月・祝)14:15 ~ 14:30</p>
+                            <p className="txt comedy-deil">五橋キャンパス押川記念ホール</p>
                         </div>
                     </div>
                 </ScrollRevealContainer>
@@ -66,8 +66,8 @@ export default function Home() {
                         <div className="explanation">
                             <p className="talent-name">suzunaokada</p>
                             <p className="txt isiyama-deil">Singer Song Dancer｜歌 × 踊🇯🇵<br/>【2001年 12月 26日　生まれ｜東京都江戸川区　出身】<br/>ハスキーボイスを強みとし、&quot;歌 × 踊&quot;のコラボレーションを夢見る新生アーティスト。<br/>身体の柔軟性やグルーヴのある動きが特徴的なコンテンポラリーダンサー謙シンガー。<br/>ファッションや表情からは独特な雰囲気を感じさせる、不思議な世界観の持ち主。<br/>■ プロフィール<br/>ダンス歴は16年。コンテンポラリーを主ジャンルとし、2024年から本格的に歌の発信をはじめる。<br/>ダンス×歌を掛け合わせた唯一無二の色を持つアーティストとして現在活動中。<br/>■ 経歴<br/>・Anly 『Alive official video』MV出演<br/>・WORLD OF DANCE 2023   best4<br/>・岡本カウアン 大阪ツアー バックダンサー<br/>・NOA &quot;NO.A&quot; TOUR SPECIAL LIVE IN ARIAKE <br/>ARENA バックダンサー</p>
-                            {/* <p className="txt comedy-deil">10月14日(月・祝)14:15 ~ 15:30</p>
-                            <p className="txt comedy-deil">五橋キャンパス押川記念ホール</p> */}
+                            <p className="txt comedy-deil">10月14日(月・祝)14:30 ~ 15:30</p>
+                            <p className="txt comedy-deil">五橋キャンパス押川記念ホール</p>
                         </div>
                     </div>
                 </ScrollRevealContainer>
@@ -101,7 +101,7 @@ export default function Home() {
                 </div>
                 <div className="explanation">
                     <p className="txt comedy-deil">10月27日(日)13:00 ~ 13:45</p>
-                    <p className="txt comedy-deil">特設ステージ(雨天時は8号館5階押川記念ホール)</p>
+                    <p className="txt comedy-deil">特設ステージ(雨天時は8号館5階ホール)</p>
                 </div>
                 <div className="yokonarabi">
                     <div className="tutigei1">
