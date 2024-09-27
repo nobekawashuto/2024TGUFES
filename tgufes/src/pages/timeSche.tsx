e3ebc494cda7fa5d664f78f08a6c82a97e92d931
nobekawashuto <maybeWinn1ble@gmail.com>
2024-09-27 15:47:29 +0900

    スケジュールページのモバイル対応です

--- tgufes/src/pages/timeSche.tsx
+++ tgufes/src/pages/timeSche.tsx
@@ -14,13 +14,13 @@ export default function Home() {
         <div className="sche-bg">
             <h2 className="sns-title txt">五橋キャンパス祭タイムスケジュール</h2>
             <div className="timeimg">
-                <Image src="/images/itutu-time.png"alt=""width={917}height={651}/>
+                <Image src="/images/itutu-time.png"alt=""width={917}height={651}className="timeImg"/>
             </div>
         </div>
         <div className="sche-bg">
             <h2 className="sns-title txt">六軒丁祭タイムスケジュール</h2>
             <div className="timeimg">
-                <Image src="/images/tuti-time.png"alt=""width={917}height={651}/>
+                <Image src="/images/tuti-time.png"alt=""width={917}height={651}className="timeImg"/>
             </div>
         </div>
         <Footer/>
